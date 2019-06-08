34ddced3eab7da33e1528f854502936e4d147442
Dmitrii Yurkovskii <tonorio@yandex.ru>
2019-06-08 20:22:21 +0300

    Update flag.py

--- flag.py
+++ flag.py
@@ -8,17 +8,17 @@ class ArgumentError(Exception):
 
 def flag(n):
     if n % 2 == 0:
-        # Верхняя граница
+        # Top border
         s1 = "#" * (3 * n + 2)
-        # Заполнение флага до центрального рисунка
+        # Flag for central image
         cycle1 = int(round(n / 2))
         s2 = ""
         while cycle1 != 0:
             s2 = s2 + "#" + " " * (3 * n) + "#" + '\n'
             cycle1 = cycle1 - 1
-        # Первая строка центрального рисунка
+        # First string central image
         s2 = s2 + "#" + " " * int(1.5 * n - 1) + "**" + " " * int(1.5 * n - 1) + "#"
-        # Строки центрального рисунка увеличение
+        # Strings central image (zoom +)
         cycle2 = (n - 2) / 2
         s3 = ""
         max_o = n - 2
@@ -28,7 +28,7 @@ def flag(n):
                 1.5 * n - 1 - dop_o / 2) + "#" + '\n'
             cycle2 = cycle2 - 1
             dop_o = dop_o + 2
-        # Строки центрального рисунка уменьшение
+        # Strings central image (zoom -)
         cycle2 = (n - 2) / 2
         dop_o = dop_o - 2
         while cycle2 != 0:
@@ -36,15 +36,15 @@ def flag(n):
                 1.5 * n - 1 - dop_o / 2) + "#" + '\n'
             cycle2 = cycle2 - 1
             dop_o = dop_o - 2
-        # Последняя строка центрального рисунка
+        # Last string central image
         s3 = s3 + "#" + " " * int(1.5 * n - 1) + "**" + " " * int(1.5 * n - 1) + "#"
-        # Заполнение флага после центрального рисунка
+        # Flag after central image
         cycle3 = int(round(n / 2))
         s4 = ""
         while cycle3 != 0:
             s4 = s4 + "#" + " " * (3 * n) + "#" + '\n'
             cycle3 = cycle3 - 1
-        # Нижняя граница
+        # Bottom border
         s4 = s4 + "#" * (3 * n + 2)
 
         s_print = s1 + '\n' + s2 + '\n' + s3 + '\n' + s4
